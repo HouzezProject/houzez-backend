0b42ac24c6cf03b9996bd590f2703d9e5566d364
Kelozheng <100518138+Kelozheng@users.noreply.github.com>
2022-09-13 14:45:12 +1000

    Add files via upload

---
+++ src/main/java/com/eta/Houzezbackend/HouzezBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eta.Houzezbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouzezBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouzezBackendApplication.class, args);
+	}
+
+}
