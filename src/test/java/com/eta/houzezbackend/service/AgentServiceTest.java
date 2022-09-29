63cb29aae9c8edcdae68038bd1ff2c74b5c3a775
chester <chesterhu0008@gmail.com>
2022-09-29 20:15:13 +1000

    HZ-10 update test;

--- src/test/java/com/eta/houzezbackend/service/AgentServiceTest.java
+++ src/test/java/com/eta/houzezbackend/service/AgentServiceTest.java
@@ -8,6 +8,8 @@ import com.eta.houzezbackend.repository.AgentRepository;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+
+
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
